5d00c08005b3a8b8010844f408bc6d6f49eaff65
Kanakaraja-2000 <130144889+Kanakaraja-2000@users.noreply.github.com>
2023-04-10 17:03:42 +0530

    Update streamlit_app.py
    Added new text line and emojis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New healthy dinner')
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 omega 3 & Blueberry Oat meal')
-streamlit.text(' 🐔 kale,spinach & Rocket smoothie')
-streamlit.text('🥑🍞Hard-boiled free range egg')
+streamlit.header(' Breakfast Menu')
+streamlit.text('🥣  omega 3 & Blueberry Oat meal')
+streamlit.text(' 🥗 kale,spinach & Rocket smoothie')
+streamlit.text(' 🐔Hard-boiled free range egg')
+streamlit.text('🥑🍞 Avacado toast')
